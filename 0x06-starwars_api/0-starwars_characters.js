be1e18ae0e2b47b4c95281d10f6500d817defc6c
mooreArrqs <amandanxumalo921@gmail.com>
2024-09-05 08:06:46 -0700

    Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+// A script that prints all characters of a Star Wars movie using the Star Wars API
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  // Loop through each character URL and print their names
+  characters.forEach(characterUrl => {
+    request(characterUrl, (err, res, charBody) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const character = JSON.parse(charBody);
+      console.log(character.name);
+    });
+  });
+});
